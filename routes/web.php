bd14629a6fba0f0d898ca98add044f37388001ea
jems2005 <153787301+jems2005@users.noreply.github.com>
2025-09-19 09:57:02 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\ClassroomController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about');
+});
 
-Route::resource('subjects', SubjectController::class);
 Route::resource('teachers', TeacherController::class);
+Route::resource('subjects', SubjectController::class);
 Route::resource('classrooms', ClassroomController::class);
